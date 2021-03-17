48404f46f82125cd24de7a4ebbbc453f889a385f
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-17 18:27:06 +0900

    Update rooms_helper.rb

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,20 @@
 module RoomsHelper
+
+  # def room_lists
+  #   message_box = []
+    
+  #   @messages.each do |message|
+  #     message_box.push(message)
+  #   end
+    
+  #   @message_cs.each do |message_c|
+  #     message_box.push(message_c)
+  #   end
+    
+  #   message_box.sort do |a, b|
+  #     b[:created_at] <=> a[:created_at]
+  #   end
+
+  # end
+
 end
