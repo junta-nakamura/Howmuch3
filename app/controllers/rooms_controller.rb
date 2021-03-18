a2203f81026d0a0058288c574f47001c34431a3d
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-18 23:19:42 +0900

    entry削除

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -3,37 +3,30 @@ class RoomsController < ApplicationController
 
   def index
     if company_signed_in?
-      # @companyRooms = Room.where(company_id: current_company.id)
       @companyRooms = current_company.rooms.includes(:company_messages).order("messages.created_at desc")
     elsif user_signed_in?
-      # @userRooms = Room.where(user_id: current_user.id)
       @userRooms = current_user.rooms.includes(:messages).order("messages.created_at desc")
     end
   end
 
   def create
     @room = Room.create(room_params)
-    @entryCompany = Entry.create(join_room_params)
     redirect_to room_path(@room.id)
   end
 
   def show
     @room = Room.find(params[:id])
-    if Entry.where(company_id: current_company.id, room_id: @room.id).present?
+    if Room.where(id: @room.id, company_id: current_company.id).present?
       @messages = @room.messages.includes(:user).order("created_at asc")
       @companyMessages = @room.company_messages.includes(:company).order("created_at asc")
       @message = Message.new
       @companyMessage = CompanyMessage.new
-      @entries = @room.entries
     else
       redirect_back(fallback_location: root_path)
     end
-  end
+  end   
 
   private
-  def join_room_params
-    params.require(:entry).permit(:user_id, :company_id, :room_id).merge(room_id: @room.id)
-  end
 
   def room_params
     params.require(:room).permit(:user_id, :company_id)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,19 @@ class UsersController < ApplicationController
 
   def show
     if user_signed_in?
-      @myPortfolios = Portfolio.includes(:user)
+      @myPortfolios = Portfolio.where(user_id: current_user.id)
     elsif company_signed_in?
       @user = User.find(params[:id])
-      @portfolios = Portfolio.all
-      @myPortfolios = @portfolios.where(user_id: @user.id)
-      
-      @currentCompanyEntry = Entry.where(company_id: current_company.id)
-      @userEntry = Entry.where(user_id: @user.id)
-      @currentCompanyEntry.each do |cc|
-        @userEntry.each do |u|
-          if cc.room_id == u.room_id then
-            @haveRoom = true
-            @roomId = cc.room_id
-          end
-        end
-      end 
-      unless @haveRoom
+      @myPortfolios = Portfolio.where(user_id: @user.id)
+      if @roomMatch = Room.where(company_id: current_company.id).where(user_id: @user.id)
+        @haveRoom = true
+        @roomId = @roomMatch.ids
+      else
         @room = Room.new
-        @entry = Entry.new
       end
     else
       @user = User.find(params[:id])
-      @portfolios = Portfolio.all
-      @myPortfolios = @portfolios.where(user_id: @user.id)
+      @myPortfolios = Portfolio.includes(:user)
     end
   end
 
