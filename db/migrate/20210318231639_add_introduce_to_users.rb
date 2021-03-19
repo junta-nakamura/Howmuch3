f12c56d0cea92a7b6380b3c92279ae08e43e993c
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-03-19 09:20:02 +0900

    Revert "uesrsテーブルにintroduce,typeカラム追加"
    This reverts commit 3557cefb51aa47783e797bd967926b5bdf8e75f6.

--- db/migrate/20210318231639_add_introduce_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddIntroduceToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :introduce, :text, null: false
-    add_column :users, :type, :integer, null: false
-  end
-end
