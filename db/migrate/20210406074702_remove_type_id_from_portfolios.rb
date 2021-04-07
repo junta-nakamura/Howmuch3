a79d41fa3374dbcad894dc871267cf1b026a5f2d
junta-nakamura <79129848+junta-nakamura@users.noreply.github.com>
2021-04-07 12:31:52 +0900

    デプロイエラー

--- db/migrate/20210406074702_remove_type_id_from_portfolios.rb
+++ db/migrate/20210406074702_remove_type_id_from_portfolios.rb
@@ -1,6 +1,6 @@
 class RemoveTypeIdFromPortfolios < ActiveRecord::Migration[6.0]
   def change
-    remove_column :portfolios, :type_id, :string
-    remove_column :users, :type_id, :string
+    # remove_column :portfolios, :type_id, :string
+    # remove_column :users, :type_id, :string
   end
 end
--- db/migrate/20210406080037_remove_type_id_from_users.rb
+++ db/migrate/20210406080037_remove_type_id_from_users.rb
@@ -1,5 +1,5 @@
 class RemoveTypeIdFromUsers < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :type_id, :string
+    # remove_column :users, :type_id, :string
   end
 end
